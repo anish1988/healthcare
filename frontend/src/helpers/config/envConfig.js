83acfd7ac4f53cce6f950cb4c2f3131f8ee41d79
Anish <way2anishrai@gmail.com>
2024-08-05 17:58:43 +0530

    Update envConfig.js
    update backend URL

--- frontend/src/helpers/config/envConfig.js
+++ frontend/src/helpers/config/envConfig.js
@@ -1,5 +1,5 @@
 //const url = process.env.REACT_APP_API_BASE_URL;
 
-const url = "https://doctor-appointment-web.onrender.com/api/v1";
+const url = "https://healthcare-vgt7.onrender.com/api/v1";
 
 export const getBaseUrl = () =>{return  url}
