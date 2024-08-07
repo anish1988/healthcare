0535d050fa2f0292a8d74e9088be5425414caf6f
Anish Rai <way2anishrai@gmail.com>
2024-08-07 15:56:07 +0530

    Patints Dshboard fixes

--- api/src/app/modules/appointment/appointment.route.ts
+++ api/src/app/modules/appointment/appointment.route.ts
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.get('/', AppointmentController.getAllAppointment);
 
-router.get('/patient/appointments',auth(AuthUser.PATIENT), AppointmentController.getPatientAppointmentById);
+router.post('/patient/appointments',auth(AuthUser.PATIENT), AppointmentController.getPatientAppointmentById);
 router.get('/patient/invoices',auth(AuthUser.PATIENT), AppointmentController.getPatientPaymentInfo);
 router.get('/doctor/invoices',auth(AuthUser.DOCTOR), AppointmentController.getDoctorInvoices);
 
